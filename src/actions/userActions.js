d0c6bf9304bac6b7c343cdeae758becc03056ea9
iamshiv007 <iamshiv20032003@gmail.com>
2023-05-17 10:49:58 +0530

    login, register, userDetails action completed

---
+++ src/actions/userActions.js
@@ -0,0 +1,53 @@
+import { LOAD_USER_FAIL, LOAD_USER_REQUEST, LOAD_USER_SUCCESS, LOGIN_FAIL, LOGIN_REQUEST, LOGIN_SUCCESS, REGISTER_USER_FAIL, REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS } from "../constants/userConstants";
+import axios from 'axios'
+
+// Register
+export const register = (userData) => async (dispatch) => {
+    try {
+
+        dispatch({ type: REGISTER_USER_REQUEST })
+
+        const config = { headers: { "content-type": "multipart/form-data" } }
+
+        const { data } = await axios.post(`http://localhost:5000/api/register`, userData, config)
+
+        dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user })
+
+    } catch (error) {
+        dispatch({
+            type: REGISTER_USER_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+export const login = (userData) => async (dispatch) => {
+    try {
+        dispatch({ type: LOGIN_REQUEST })
+
+        const { data } = await axios.post("http://localhost:5000/api/login", userData)
+
+        dispatch({ type: LOGIN_SUCCESS, payload: data.user })
+    } catch (error) {
+        dispatch({
+            type: LOGIN_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+
+export const loadUser = () => async (dispatch) => {
+    try {
+        dispatch({ type: LOAD_USER_REQUEST })
+
+        const { data } = await axios.get(`http://localhost:5000/api/me`)
+        dispatch({ type: LOAD_USER_SUCCESS, payload: data })
+
+    } catch (error) {
+        dispatch({
+            type: LOAD_USER_FAIL,
+            payload: error.response.data.message
+        })
+    }
+} 
