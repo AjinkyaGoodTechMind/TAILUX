07517ae52e54d68fe00b5dcf18f5cd216dd80e2d
iamshiv007 <iamshiv20032003@gmail.com>
2023-05-25 01:55:53 +0530

    Step4a completed

--- src/StudioPage/Step4a.jsx
+++ src/StudioPage/Step4a.jsx
@@ -3,90 +3,232 @@ import Tag from "../components/studio/Tag";
 import Navbar from "../components/layout/navbar/Navbar";
 import Footer from "../components/layout/footer/Footer";
 import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+
+import ProfileIcon2 from "../assets/images/ProfileIcon2.svg";
+import ShirtIcon1 from "../assets/images/ShirtIcon1.svg";
+import PantIcon1 from "../assets/images/PantIcon1.svg";
+import DeleteIcon1 from "../assets/images/DeleteIcon1.svg";
+import EditIcon1 from "../assets/images/EditIcon1.svg";
+
+import "./Step4a.css";
 import "./Studio.css";
-import React from "react";
+import FormStepper from "./FormStepper";
 
 export default function Step4b() {
   const navigate = useNavigate();
   const Previous = useNavigate();
   const Step4B = useNavigate();
 
+  const [state, setState] = useState(true);
+  const [inputStyle, setInputStyle] = useState();
+
+  const editSizesFun = () => {
+    setState(false);
+    setInputStyle({
+      background: " #E8E8E8",
+      border: "1px solid #ADADAD",
+      borderRadius: "4px",
+      color: "#3A3A3A",
+    });
+  };
+
+  const saveSizeFun = () => {
+    setState(true);
+    setInputStyle();
+  };
+
+  const sizesCancelFun = () => {
+    setState(true);
+    setInputStyle();
+  };
+
   return (
     <div id="Step-All-StudioPage">
       <Navbar />
       <Banner />
       <Tag />
-      <div id="StudioPage-Step-4"></div>
+      <FormStepper active={3} />
       <div id="Content">
-        <h1 className="Content-h1">Select Size & Fitting</h1>
+        <h3 className="Content-h1">Select Size & Fitting</h3>
 
-        <div id="Size-fit">
-          <div className="form demoSize1">
-            <div className="user"></div>
-            <input
-              type="text"
-              name=""
-              id=""
-              placeholder="Type Name here..."
-              value="Manju"
-              style={{
-                width: "40%",
-                marginTop: "30px",
-                fontWeight: "bold",
-                border: "none",
-                fontSize: "2rem",
-                textAlign: "center",
-              }}
-            />
-            <div className="shirt">
-              <form className="sizeForm" id="User-size" action="">
-                <div id="left">
-                  <label htmlFor="shoulder">Shoulder</label>
-                  <input type="number" id="shoulder demo-display" name="shoulder" value={1} disabled />
-                  <label htmlFor="chest ">Chest</label>
-                  <input type="number" id="chest demo-display" name="chest" value={1} disabled />
-                  <label htmlFor="waist">Waist</label>
-                  <input type="number" id="waist demo-display" name="waist" value={1} disabled />
+        <div>
+          <div className="mySizesContainer">
+            <div>
+              <div className="sizeBox">
+                <div>
+                  <div>
+                    <img src={ProfileIcon2} alt="ProfileIcon2" />
+                  </div>
+                  <p className="text19b">Manju</p>
                 </div>
-                <div id="right">
-                  <label htmlFor="back length">Back Length</label>
-                  <input type="number" id="backLength demo-display" name="back length" value={1} disabled />
-                  <label htmlFor="sleeves">Sleeve</label>
-                  <input type="number" id="sleeves demo-display" name="sleeves" value={1} disabled />
-                  <label htmlFor="neck">Neck</label>
-                  <input type="number" id="neck demo-display" name="neck" value={1} disabled />
+
+                <img src={ShirtIcon1} alt="ShirtIcon1" />
+
+                <div className="shirtSizeBox">
+                  <div>
+                    <div>
+                      <label htmlFor="">Shoulder </label>
+                      <input
+                        style={inputStyle}
+                        disabled={state}
+                        type="text"
+                        value="1cm"
+                      />
+                    </div>
+
+                    <div>
+                      <label htmlFor="">Chest</label>
+                      <input
+                        style={inputStyle}
+                        disabled={state}
+                        type="text"
+                        value={"2cm"}
+                      />
+                    </div>
+
+                    <div>
+                      <label htmlFor="">Waist</label>
+                      <input
+                        style={inputStyle}
+                        disabled={state}
+                        type="text"
+                        value={"3cm"}
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <div>
+                      <label htmlFor="">Back Length</label>
+                      <input
+                        style={inputStyle}
+                        disabled={state}
+                        type="text"
+                        value={"1cm"}
+                      />
+                    </div>
+
+                    <div>
+                      <label htmlFor="">Sleeve</label>
+                      <input
+                        style={inputStyle}
+                        disabled={state}
+                        type="text"
+                        value={"2cm"}
+                      />
+                    </div>
+
+                    <div>
+                      <label htmlFor="">Neck</label>
+                      <input
+                        style={inputStyle}
+                        disabled={state}
+                        type="text"
+                        value={"3cm"}
+                      />
+                    </div>
+                  </div>
                 </div>
-              </form>
+
+                <img src={PantIcon1} alt="" />
+
+                <div className="pantSizeBox">
+                  <div>
+                    <div>
+                      <label htmlFor="">Waist</label>
+                      <input
+                        style={inputStyle}
+                        disabled={state}
+                        type="text"
+                        value="1cm"
+                      />
+                    </div>
+
+                    <div>
+                      <label htmlFor="">Length</label>
+                      <input
+                        style={inputStyle}
+                        disabled={state}
+                        type="text"
+                        value={"2cm"}
+                      />
+                    </div>
+
+                    <div>
+                      <label htmlFor="">Inseam</label>
+                      <input
+                        style={inputStyle}
+                        disabled={state}
+                        type="text"
+                        value={"3cm"}
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <div>
+                      <label htmlFor="">Waist to Crotch</label>
+                      <input
+                        style={inputStyle}
+                        disabled={state}
+                        type="text"
+                        value={"1cm"}
+                      />
+                    </div>
+
+                    <div>
+                      <label htmlFor="">Upper Leg Width</label>
+                      <input
+                        style={inputStyle}
+                        disabled={state}
+                        type="text"
+                        value={"2cm"}
+                      />
+                    </div>
+
+                    <div>
+                      <label htmlFor="">Lower Leg Width</label>
+                      <input
+                        style={inputStyle}
+                        disabled={state}
+                        type="text"
+                        value={"3cm"}
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <button>
+                  <img src={DeleteIcon1} alt="DeleteIcon1" />
+                </button>
+
+                <div>
+                  <button
+                    style={state ? {} : { display: "none" }}
+                    onClick={editSizesFun}
+                  >
+                    Edit
+                    <img src={EditIcon1} alt="EditIcon1" />
+                  </button>
+
+                  <button style={state ? {} : { display: "none" }}>
+                    select
+                  </button>
+                </div>
+
+                <div style={state ? { display: "none" } : {}}>
+                  <button onClick={saveSizeFun}>Save</button>
+                  <button onClick={sizesCancelFun}>Cancel</button>
+                </div>
+              </div>
+
+              <div className="newSizeBox">
+                <button className="newSizeBtn">+</button>
+                <p>add new size</p>
+              </div>
             </div>
-            <div className="pant">
-              <form className="sizeForm" id="User-size" action="">
-                <div id="left">
-                  <label htmlFor="waist">Waist</label>
-                  <input type="number" id="waist demo-display" name="waist" value={1} disabled />
-                  <label htmlFor="length">Length</label>
-                  <input type="number" id="length demo-display" name="length" value={1} disabled />
-                  <label htmlFor="inseam">Inseam</label>
-                  <input type="number" id="inseam demo-display" name="inseam" value={1} disabled />
-                </div>
-                <div id="right">
-                  <label htmlFor="waist2crotch">Waist to Crotch</label>
-                  <input type="number" id="waist2crotch demo-display" name="waist2crotch" value={1} disabled />
-                  <label htmlFor="upperlegwidth">Upper Leg Width</label>
-                  <input type="number" id="upperlegwidth demo-display" name="upperlegwidth" value={1} disabled />
-                  <label htmlFor="LowerlegWidth">Lower Leg Width</label>
-                  <input type="number" id="LowerlegWidth demo-display" name="LowerlegWidth" value={1} disabled />
-                </div>
-              </form>
-            </div>
-            <button type="submit" id="addBtn">
-              Selected
-            </button>
           </div>
-        </div>
-
-        <div id="Add-Style" onClick={() => Step4B("/step4b")}>
-          <div>+</div>
-          <span>Add Style</span>
         </div>
       </div>
       <div className="Buttons">
